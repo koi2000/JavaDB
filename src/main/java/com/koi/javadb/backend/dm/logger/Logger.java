228b9a8c7d3d303c9d905b2b245711a77072315c
koi2000 <koi20000@163.com>
2023-07-20 15:14:16 +0800

    feat: 完成Logger

---
+++ src/main/java/com/koi/javadb/backend/dm/logger/Logger.java
@@ -0,0 +1,88 @@
+package com.koi.javadb.backend.dm.logger;
+
+import com.koi.javadb.backend.utils.Panic;
+import com.koi.javadb.backend.utils.Parser;
+import com.koi.javadb.common.Errors;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/*
+* 日志的二进制文件
+* [XChecksum][Log1][Log2][Log3]...[LogN][BadTail]
+* Size是一个四字节整数，标识Data段的字节数，Checksum 则是该条日志的校验和
+* [Size][Checksum][Data]
+*
+*
+*
+*
+*
+* * * * * * * * */
+public interface Logger {
+
+    void log(byte[] data);
+    void truncate(long x) throws Exception;
+    byte[] next();
+    void rewind();
+    void close();
+
+    public static Logger create(String path){
+        File f = new File(path + LoggerImpl.LOG_SUFFIX);
+        try{
+            if (!f.createNewFile()){
+                Panic.panic(Errors.FileExistsException);
+            }
+        }catch (IOException e){
+            Panic.panic(e);
+        }
+        if(!f.canRead() || !f.canWrite()) {
+            Panic.panic(Errors.FileCannotRWException);
+        }
+
+        FileChannel fc = null;
+        RandomAccessFile raf = null;
+        try {
+            raf = new RandomAccessFile(f, "rw");
+            fc = raf.getChannel();
+        } catch (FileNotFoundException e) {
+            Panic.panic(e);
+        }
+        // 将校验和放进去，初始值为0
+        ByteBuffer buf = ByteBuffer.wrap(Parser.int2Byte(0));
+        try {
+            fc.position(0);
+            fc.write(buf);
+            fc.force(false);
+        } catch (IOException e) {
+            Panic.panic(e);
+        }
+
+        return new LoggerImpl(raf, fc, 0);
+    }
+
+    public static Logger open(String path){
+        File f = new File(path + LoggerImpl.LOG_SUFFIX);
+        if (!f.exists()){
+            Panic.panic(Errors.FileExistsException);
+        }
+        if(!f.canRead() || !f.canWrite()) {
+            Panic.panic(Errors.FileCannotRWException);
+        }
+
+        FileChannel fc = null;
+        RandomAccessFile raf = null;
+        try {
+            raf = new RandomAccessFile(f, "rw");
+            fc = raf.getChannel();
+        } catch (FileNotFoundException e) {
+            Panic.panic(e);
+        }
+        LoggerImpl lg = new LoggerImpl(raf, fc);
+        lg.init();
+        return lg;
+    }
+}
---
+++ src/main/java/com/koi/javadb/backend/dm/logger/LoggerImpl.java
@@ -0,0 +1,225 @@
+package com.koi.javadb.backend.dm.logger;
+
+/*
+ * 日志文件读写
+ * 日志文件标准格式为：
+ * [XChecksum] [Log1] [Log2] ... [LogN] [BadTail]
+ * XChecksum 为后续所有日志计算的Checksum，int类型
+ *
+ * 每条正确日志的格式为：
+ * [Size] [Checksum] [Data]
+ * Size 4字节int 标识Data长度
+ * Checksum 4字节int
+ * * * * * * * * * * */
+
+import com.google.common.primitives.Bytes;
+import com.koi.javadb.backend.utils.Panic;
+import com.koi.javadb.backend.utils.Parser;
+import com.koi.javadb.common.Errors;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.Arrays;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class LoggerImpl implements Logger {
+    private static final int SEED = 13331;
+    private static final int OF_SIZE = 0;
+    private static final int OF_CHECKSUM = OF_SIZE + 4;
+    private static final int OF_DATA = OF_CHECKSUM + 4;
+
+    public static final String LOG_SUFFIX = ".log";
+
+    private RandomAccessFile file;
+    private FileChannel fc;
+    private Lock lock;
+
+    // 当前日志指针的位置
+    private long position;
+    // 初始化时记录，log操作不更新
+    private long fileSize;
+    private int xChecksum;
+
+    LoggerImpl(RandomAccessFile raf, FileChannel fc) {
+        this.file = raf;
+        this.fc = fc;
+        lock = new ReentrantLock();
+    }
+
+    LoggerImpl(RandomAccessFile raf, FileChannel fc, int xChecksum) {
+        this.file = raf;
+        this.fc = fc;
+        this.xChecksum = xChecksum;
+        lock = new ReentrantLock();
+    }
+
+    void init() {
+        long size = 0;
+        try {
+            size = file.length();
+        } catch (IOException e) {
+            Panic.panic(e);
+        }
+        // 计算size，将size写入
+        ByteBuffer raw = ByteBuffer.allocate(4);
+        try {
+            fc.position(0);
+            fc.read(raw);
+        } catch (IOException e) {
+            Panic.panic(e);
+        }
+        // 计算校验和
+        int xChecksum = Parser.parseInt(raw.array());
+        this.fileSize = size;
+        this.xChecksum = xChecksum;
+        checkAndRemoveTail();
+    }
+
+    // 检查并移除bad tail
+    private void checkAndRemoveTail() {
+        rewind();
+        int xCheck = 0;
+        while (true) {
+            byte[] log = internNext();
+            if (log==null)break;
+            xCheck = calChecksum(xCheck, log);
+        }
+        if(xCheck!=xChecksum){
+            Panic.panic(Errors.BadLogFileException);
+        }
+        try {
+            // position的数据被剔除
+            truncate(position);
+        }catch (Exception e){
+            Panic.panic(e);
+        }
+        try{
+            // 将文件指针转回到该位置
+            file.seek(position);
+        }catch (IOException e){
+            Panic.panic(e);
+        }
+        rewind();
+    }
+
+    @Override
+    public void log(byte[] data) {
+        byte[] log = wrapLog(data);
+        ByteBuffer buf = ByteBuffer.wrap(log);
+        lock.lock();
+        try {
+            fc.position(fc.size());
+            fc.write(buf);
+        } catch (IOException e) {
+            Panic.panic(e);
+        } finally {
+            lock.unlock();
+        }
+        updateXChecksum(log);
+    }
+
+    private void updateXChecksum(byte[] log) {
+        this.xChecksum = calChecksum(this.xChecksum, log);
+        try {
+            fc.position(0);
+            fc.write(ByteBuffer.wrap(Parser.int2Byte(xChecksum)));
+            fc.force(false);
+        } catch (IOException e) {
+            Panic.panic(e);
+        }
+    }
+
+    // 将data封装一下，将两个部分组装在一起
+    private byte[] wrapLog(byte[] data) {
+        byte[] checksum = Parser.int2Byte(calChecksum(0, data));
+        byte[] size = Parser.int2Byte(data.length);
+        return Bytes.concat(size, checksum, data);
+    }
+
+    @Override
+    public void truncate(long x) throws Exception {
+        lock.lock();
+        try {
+            fc.truncate(x);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public byte[] next() {
+        lock.lock();
+        try {
+            byte[] log = internNext();
+            if (log == null) return null;
+            // 将log数组拷贝到指定位置
+            return Arrays.copyOfRange(log, OF_DATA, log.length);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    /*
+    * 读出size大小
+    * 读出数据
+    * * * */
+    private byte[] internNext() {
+        if (position + OF_DATA >= fileSize) {
+            return null;
+        }
+        ByteBuffer tmp = ByteBuffer.allocate(4);
+        try {
+            fc.position(position);
+            fc.read(tmp);
+        } catch (IOException e) {
+            Panic.panic(e);
+        }
+        int size = Parser.parseInt(tmp.array());
+        if (position + size + OF_DATA > fileSize) {
+            return null;
+        }
+        ByteBuffer buf = ByteBuffer.allocate(OF_DATA + size);
+        try {
+            fc.position(position);
+            fc.read(buf);
+        } catch (IOException e) {
+            Panic.panic(e);
+        }
+        byte[] log = buf.array();
+        // 计算log数组的校验和
+        int checkSum1 = calChecksum(0, Arrays.copyOfRange(log, OF_DATA, log.length));
+        // 读出校验和
+        int checkSum2 = Parser.parseInt(Arrays.copyOfRange(log, OF_CHECKSUM, OF_DATA));
+        if (checkSum1 != checkSum2) {
+            return null;
+        }
+        position += log.length;
+        return log;
+    }
+
+    // 计算校验和
+    private int calChecksum(int xCheck, byte[] log) {
+        for (byte b : log) {
+            xCheck = xCheck * SEED + b;
+        }
+        return xCheck;
+    }
+
+    @Override
+    public void rewind() {
+        position = 4;
+    }
+
+    @Override
+    public void close() {
+        try {
+            fc.close();
+            file.close();
+        } catch (IOException e) {
+            Panic.panic(e);
+        }
+    }
+}
